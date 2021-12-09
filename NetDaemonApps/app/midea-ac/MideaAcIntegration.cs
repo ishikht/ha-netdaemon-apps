20da88414d13c150bde6e71176fd060bcfe5430d
ishikht <ishikht@gmail.com>
2021-12-09 18:33:10 +0200

    MideaAC: Added empty app

---
+++ NetDaemonApps/app/midea-ac/MideaAcIntegration.cs
@@ -0,0 +1,12 @@
+﻿using NetDaemon.Common.Reactive;
+
+namespace MideaAcIntegration
+{
+    public class MideaAcIntegration: NetDaemonRxApp
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
